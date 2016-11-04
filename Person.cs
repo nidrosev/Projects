a18b503d97154e7056bb8d224bd3141b3792b748
nidrosev <nidrosev@gmail.com>
2016-11-04 09:33:02 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Person
+    {
+        public string name;
+        public byte age;
+
+        private static int mCurrentGlobalId;
+
+        public int Id { get; set; }
+
+        public Person()
+        {
+            Id = mCurrentGlobalId++;
+            this.name = "No name";
+            this.age = 1;
+
+            // Console.WriteLine("Name : {0} , Age : {1}", this.name, this.age);
+
+        }
+        public Person(string name) : this()
+        {
+            this.name = name;
+            // Console.WriteLine("Name i : {0} ,Age is {1}", this.name, this.age);
+
+        }
+        public Person(string name, byte age) : this(name)
+        {
+            this.age = age;
+            //Console.WriteLine("Name i : {0}, Age is {1}", this.name, this.age);
+
+        }
+
+        public void  EnterData()
+        {
+            string PersonString = "";
+            List<string> personData = new List<string>();
+            var people = new List<Person>();
+            do
+            {
+                Console.WriteLine("Enter Person data:");
+                PersonString = Console.ReadLine();
+                if (PersonString != "quit")
+                {
+                    people.Add(this.ParseData(PersonString));
+                }
+
+            } while (PersonString != "quit");
+
+            this.PeopleSort(people);
+            
+
+        }
+
+        public Person ParseData(string PersonString)
+        {
+            
+                string[] person = PersonString.Split(new string[] { "//" }, StringSplitOptions.RemoveEmptyEntries);
+                var p = new Person(person[0], byte.Parse(person[1]));
+            
+            return p;
+           
+        }
+
+        public void PeopleSort(List<Person> peopleResult)
+        {
+
+            peopleResult = peopleResult.Where(x => x.age > 18).OrderByDescending(x => x.name.Length).ToList<Person>();
+            foreach (var item in peopleResult)
+            {
+                Console.WriteLine("Name is {0} , Age is {1}", item.name, item.age);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter size of array");
+            int sizeArray = 0;
+         
+            int result;
+            //int sizeArray = Int32.Parse(Console.ReadLine());
+            while (!Int32.TryParse(Console.ReadLine(),out sizeArray));
+            Program.FindElement(sizeArray);
+        }
+
+        public static void FindElement(int sizeArray)
+        {
+            int[][] myArray = new int[sizeArray][];
+            Console.WriteLine("sizeArray {0}", sizeArray);
+            int k = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = new int[sizeArray];
+                for (int j = 0; j < myArray.Length; j++)
+                {
+                    k++;
+                    myArray[i][j] = k;
+
+                }
+
+            }
+            Console.WriteLine("Enter  First and Second number");
+            int FirstNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            int SecondNumber = Int32.Parse(Console.ReadLine());
+
+            // while (!Int32.TryParse(myArray[FirstNumber][SecondNumber], out result));
+            try
+            {
+                Console.WriteLine("The result is:  {0}", myArray[FirstNumber][SecondNumber]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Error please try again");
+            }
+
+            //Console.WriteLine("The result is:  {0}", myArray[FirstNumber][SecondNumber]);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
